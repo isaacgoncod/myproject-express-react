edbe17907f8554488db5caa2e5fa25bdc809cb4d
isaacgoncod <isaacgoncod@gmail.com>
2023-02-23 21:20:21 -0300

    Conhecendo o props

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import "./App.css";
-import HelloWorld from "./components/HelloWorld";
+import SayMyName from "./components/SayMyName";
+import Pessoa from "./components/Pessoa";
 
 function App() {
-  const name = "Matheus";
-
-  const newName = name.toUpperCase();
-
-  function soma(a, b) {
-    return a + b;
-  }
-
-  const url = "https://via.placeholder.com/150";
-
+  const nome = "Welli";
   return (
     <div className="App">
-      <h1>Alterando JSX</h1>
-      <p>Olá, {newName}</p>
-      <p>soma: {soma(1, 2)}</p>
-      <img src={url} alt="Minha Imagem" />
-      <HelloWorld />
+      <SayMyName nome="Isaac" />
+      <SayMyName nome="Gustavo" />
+      <SayMyName nome="Bruno" />
+      <SayMyName nome={nome} />
+      <Pessoa nome="Isaac" idade="21" profissao="Programador" />
     </div>
   );
 }
